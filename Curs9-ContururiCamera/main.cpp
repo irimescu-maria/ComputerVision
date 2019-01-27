ef3a31012b7831e9d69971a58a58a3308b379b78
irimescu-maria <irimescu.mariaalexandra@gmail.com>
2019-01-27 09:20:01 +0200

    Contururi camera WEB

--- Curs9-ContururiCamera/main.cpp
+++ Curs9-ContururiCamera/main.cpp
@@ -1,8 +1,39 @@
 #include <QCoreApplication>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+void contururiCamera(Mat& frame, Mat& contururi){
+    cvtColor(frame, contururi, COLOR_BGR2GRAY);
+    GaussianBlur(contururi, contururi, Size(7, 7), 1.5, 1.5);
+    Canny(contururi, contururi, 0, 30, 3);
+    imshow("Contururi camera", contururi);
+}
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    VideoCapture cap(0); //open the default camera
 
-    return a.exec();
+    if(!cap.isOpened()){ // daca nu se deschide camera, incheiem programul
+        cout<<"Camera web nu poate fi pornita."<<endl;
+        return -1;
+    }
+
+    Mat camera;
+    namedWindow("Redare camera", WINDOW_AUTOSIZE);
+    while(true){
+
+        Mat frame;
+        cap>>frame;
+        contururiCamera(frame, camera);
+        if(waitKey(30) >= 0)
+            break;
+    }
+
+    waitKey(0);
+    return 0;
 }
